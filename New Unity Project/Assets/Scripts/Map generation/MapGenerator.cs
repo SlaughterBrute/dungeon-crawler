ac4aca73a70703f2226347ca951989eb6c718cbd
SimonTer <1mappat1@gmail.com>
2021-08-07 15:18:17 +0200

    Can modify Noisemap lacunarity. Noicemap have seed
    Changed  sprite size from 100 to 16 pixels to correctly work with tilemaps and gridsystems. Changed "SampleScene" cell size from 0.16 to 1 to work with the "resized" sprites.

--- New Unity Project/Assets/Scripts/Map generation/MapGenerator.cs
+++ New Unity Project/Assets/Scripts/Map generation/MapGenerator.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [SerializeField]
-    private int mapWidth;
-    [SerializeField]
-    private int mapHeight;
-    [SerializeField]
-    private float noiseScale;
+    [SerializeField] private int mapWidth;
+    [SerializeField] private int mapHeight;
+    [SerializeField] private float noiseScale;
     private DisplayMap DisplayMapReference;
     public bool autoUpdateMap;
-    [SerializeField]
-    private float x;
-    [SerializeField]
-    private float y;
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+    [SerializeField][Range(1,5)] private int octaves;
+    [SerializeField][Range(0,1)] private float persistance;
+    [SerializeField] private float lacunarity; // Detail for each octave
+    [SerializeField] private string seed;
 
+    public void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if(octaves < 1)
+        {
+            octaves = 1;
+        }
+    }
 
     public void GenerateMap()
     {
-        float[,] noiceMap = NoiseGenerator.GenerateNoiceMap(mapWidth, mapHeight, noiseScale, x, y);
+        float[,] noiceMap = NoiseGenerator.GenerateNoiceMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity, seed, x, y);
         DisplayMapReference = FindObjectOfType<DisplayMap>();
         DisplayMapReference.DrawMap(noiceMap);
     }
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GenerateMap();
+        x += 1 * Time.deltaTime;
     }
 }
--- New Unity Project/Assets/Scripts/Map generation/NoiseGenerator.cs
+++ New Unity Project/Assets/Scripts/Map generation/NoiseGenerator.cs
@@ -4,29 +4,76 @@ using UnityEngine;
 
 public static class NoiseGenerator
 {
-    public static float[,] GenerateNoiceMap(int mapWidth, int mapHeight, float scale, float xpos, float ypos)
-    {
+    public static float[,] GenerateNoiceMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity, string seed, float xpos, float ypos)
+    {   //octaves general shape
+        //lacunarity small details in octaves
+        //persistance lacunaritys influence on octaves
         float[,] map = new float[mapWidth, mapHeight];
         if (scale <= 0)
         {
             scale = 0.0001f;
         }
 
+        int seedNumber = seed.GetHashCode();
+        Vector2[] positionOffcet = new Vector2[octaves];
+        System.Random randGen = new System.Random(seedNumber);
+        for(int i = 0; i < octaves; i++)
+        {
+            positionOffcet[i].x = randGen.Next(-80000, 80000) + xpos;
+            positionOffcet[i].y = randGen.Next(-80000, 80000) + ypos;
+        }
+
+        float halfMapWidth = mapWidth / 2f;
+        float halfMapHeight = mapHeight / 2f;
+
+        float maxHeight = float.MinValue;
+        float minHeight = float.MaxValue;
+
         for(int x = 0; x < mapWidth; x++)
         {
             for (int y = 0; y < mapHeight; y++)
             {
-                float sampleX = x / scale;
-                sampleX += xpos;
-                float sampleY = y / scale;
-                sampleY += ypos;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0; 
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
-                map[x, y] = perlinValue;
+                for(int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x-halfMapWidth) / scale * frequency;
+                    sampleX += positionOffcet[i].x * Mathf.Pow(lacunarity, i);
+                    float sampleY = (y-halfMapHeight) / scale * frequency;
+                    sampleY += positionOffcet[i].y * Mathf.Pow(lacunarity, i);
+
+                    //get perlin value
+                    float perlinValue = (Mathf.PerlinNoise(sampleX, sampleY) * 2) - 1;
+                    noiseHeight += perlinValue * amplitude;
+                    //prep for next octave
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+
+                    
+                }
+                if(noiseHeight < minHeight)
+                {
+                    minHeight = noiseHeight;
+                }
+                else if(noiseHeight > maxHeight)
+                {
+                    maxHeight = noiseHeight;
+                }
+                map[x, y] = noiseHeight;
             }
         }
 
-        return map;
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                map[x, y] = Mathf.InverseLerp(minHeight, maxHeight, map[x, y]);
+            }
+        }
+        
+                return map;
     }
 
     public static float[,] GenerateNoiceMapForChunk()
